faef02a550184e54dc2fa0a7d47ce24bddaf54cd
Yann Pedron <yann-pedron@hotmail.fr>
2020-03-10 09:16:35 +0100

    contact responsive

--- view/frontend/contact.php
+++ view/frontend/contact.php
@@ -3,18 +3,12 @@
 <?php $metaDescription = 'Prenez contact avec l\'auteur de romans d\'aventures à succèss Jean Forteroche'; ?>
 
 <div class="container-fluid pt-3">
-    <!--Section: Content-->
     <section class="" 
     style="background-image: url('http://localhost:8888/public/images/rana-sawalha-W_-6PWGbYaU-unsplash.jpg'); background-size: cover; background-position: center center;">
-
-        <div class="rgba-black-strong rounded px-5 py-3">
-
-            <!-- Section heading -->
-            <!-- <h1 class="h3 text-center font-weight-bold text-white mt-3 mb-5">Contact</h1> -->
-
-            <form class="mx-md-5" action="">
+        <div class="rgba-black-strong rounded px-md-5 py-md-3 px-lg-5 py-lg-3">
+            <form class="mx-lg-5" action="">
                 <div class="row">
-                    <div class="col-md-6 mb-4">
+                    <div class="col-md-12 col-lg-6 mb-4">
                         <div class="card">
                             <h1 class="h5 text-center text-uppercase font-weight-bold pt-4">Contact</h1>
 
@@ -44,7 +38,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-5 offset-md-1 text-center white-text my-auto">
+                    <div class="col-lg-5 offset-lg-1 text-center white-text my-auto">
                         <h5 class="font-weight-bold">Adresse</h5>
                         <p class="mb-0">10 Rue d'édition</p>
                         <p class="mb-0">75000 Paris</p>
@@ -60,5 +54,4 @@
             </form>
         </div>
     </section>
-    <!--Section: Content-->
 </div>
